4c22e05dbb48f27cdb6a9cdffcf76f6c817ecde7
sunxuia <ntsunxu@163.com>
2019-09-08 12:25:47 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mvn clean test
+
+if [[ $TRAVIS_PULL_REQUEST == "false" || $TRAVIS_PULL_REQUEST_SLUG == $TRAVIS_REPO_SLUG ]]; then
+    mvn mvn org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=sunxuia_website-user
+    echo "end sonarcloud scan"
+fi
+
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
+    echo "deploy project productions"
+
+    docker login -u $docker_username -p $docker_password
+    mvn deploy -Dmaven.test.skip=true -Dgithub.global.oauth2Token=$github_token
+fi
