ab56281d1884fd8e2e8240c898fbaaf0f13d1ad2
sunxuia <ntsunxu@163.com>
2019-09-14 16:42:17 +0800

    update travis settings

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mvn clean test
+mvn clean test -q
 
 if [[ $TRAVIS_PULL_REQUEST == "false" || $TRAVIS_PULL_REQUEST_SLUG == $TRAVIS_REPO_SLUG ]]; then
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=sunxuia_website-user
-    echo "end sonarcloud scan"
+    echo "start sonarcloud scan"
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=sunxuia_website-user \
+     -q -Dmaven.test.skip=true
 fi
 
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
     echo "deploy project productions"
-
     docker login -u $docker_username -p $docker_password
-    mvn deploy -Dmaven.test.skip=true -Dgithub.global.oauth2Token=$github_token
+    mvn deploy -Dmaven.test.skip=true -Dgithub.global.oauth2Token=$github_token -q
 fi
